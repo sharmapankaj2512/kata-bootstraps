1e416f125c3e5617629b67a61e8a20a6656d59e1
SafiyaHashmi12 <shas@hellofresh.com>
2020-08-19 14:43:34 +0200

    mob next [ci-skip]

--- golang/testify/game_test.go
+++ golang/testify/game_test.go
@@ -1,2 +1,17 @@
 package kata
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGame(t *testing.T) {
+  game := &game{}
+
+  game.Roll(4)
+  assert.Equal(t, 4, game.Score())
+
+
+}
+// TODO: validate the amount 0-10
+// TODO: keep track of the score
