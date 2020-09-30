ab7d382f453e879f7e0ab593a60a58e8bdcdde2a
Frederico Wuerges Becker <fred.wuerges@gmail.com>
2020-09-30 14:41:25 +0200

    mob next [ci-skip]

--- python/pytest/life.py
+++ python/pytest/life.py
@@ -2,3 +2,4 @@ class Life():
 
     def __init__(self, grid_width):
         self.grid_width = grid_width
+        self.num_alive = 1
--- python/pytest/test_thing.py
+++ python/pytest/test_thing.py
@@ -1,7 +1,17 @@
 from life import Life
 import unittest
 
+# Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+# Any live cell with two or three live neighbours lives on to the next generation.
+# Any live cell with more than three live neighbours dies, as if by overpopulation.
+# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+
 class TestLife(unittest.TestCase):
     def test_life_exists(self):
         life = Life(3)
         self.assertEqual(3, life.grid_width)
+
+    def test_a_grid_with_one_live_cell(self):
+        life = Life(3)
+        self.assertEqual(1, life.num_alive)
