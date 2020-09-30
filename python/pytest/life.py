5b799bbb0f72b1cd619dbf9286bd21ab367aacd4
TomKealy <tom.kealy.tk@gmail.com>
2020-09-30 14:51:57 +0200

    mob next [ci-skip]

--- python/pytest/life.py
+++ python/pytest/life.py
@@ -1,5 +1,11 @@
+import numpy as np
+
+
 class Life():
 
     def __init__(self, grid_width):
         self.grid_width = grid_width
         self.num_alive = 1
+        self.grid = np.array(grid_width, grid_width)
+
+
