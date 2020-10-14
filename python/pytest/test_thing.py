cfcf6eae871eb2b1b0d74ae9acb69f04185cd87d
Tim Bleck <tbl@hellofresh.com>
2020-10-14 14:40:27 +0200

    mob next [ci-skip]

--- python/pytest/test_thing.py
+++ python/pytest/test_thing.py
@@ -1,10 +1,24 @@
 from thing import Thing
 
 
-def test_correct_input_1():
+def test_correct_input_single_digits():
     thing = Thing("I")
-
     assert thing.to_arabic() == 1
+
+    thing = Thing("V")
+    assert thing.to_arabic() == 5
+
+    thing = Thing("X")
+    assert thing.to_arabic() == 10
+
+    thing = Thing("L")
+    assert thing.to_arabic() == 50
+
+    thing = Thing("C")
+    assert thing.to_arabic() == 100
+
+    thing = Thing("M")
+    assert thing.to_arabic() == 1000
 
 
 # def test_fail():
