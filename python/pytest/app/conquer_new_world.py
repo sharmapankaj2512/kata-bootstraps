1a2bb202b1d48d163346f6c208ff8d519bcc8366
dmitry.kruglov <kruglov.dima@gmail.com>
2020-10-28 15:18:21 +0100

    mob next [ci-skip]

--- python/pytest/app/conquer_new_world.py
+++ python/pytest/app/conquer_new_world.py
@@ -1,6 +1,24 @@
+import random
+
+#
+#   Stubs of service implementation
+#
+
+def get_circumference(planet_name):
+    return random.randint()
+
+def get_fuel(distance_km):
+    return distance_km * 1.5
+
+def get_seeds(distance_km):
+    return distance_km * 5
 
 
 def conquer_new_world(planet_name):
-    circumference = get_circumference(planet_name) #
+    distance_km = get_circumference(planet_name) #
     fuel = get_fuel(distance_km)
     seeds = get_seeds(distance_km)
+
+    #
+    #
+    #
