866ec2a04163946d2e319d4de1b59b17468bb0d5
Érica Hidemi Mitsuishi <emit@hellofresh.com>
2020-11-11 15:25:57 +0100

    mob next [ci-skip]

--- js/jest-tests/test/alexa.test.js
+++ js/jest-tests/test/alexa.test.js
@@ -4,25 +4,36 @@
 
 function helloAlexa(output, day) {
   output.say("Good Morning");
-  if (day == "Mon")  output.say("have a great start to the week");
-
+  if (day == "Mon") output.say("have a great start to the week");
+  if (day == "Fri") output.say("hope you have great plan for the weekend");
 }
 
 describe("Alexa", () => {
   it("says good morning", () => {
-
-    const output = { say:  jest.fn() };
+    const output = { say: jest.fn() };
     helloAlexa(output, "Sun");
     expect(output.say).toHaveBeenLastCalledWith("Good Morning");
   });
 
-  it("on monday it says good morning, have a great start to the week", () => { 
-    const output = { say:  jest.fn() };
-    const day = { say:  jest.fn() };
+  it("on monday it says good morning, have a great start to the week", () => {
+    const output = { say: jest.fn() };
+    const day = { say: jest.fn() };
 
     helloAlexa(output, "Mon");
     expect(output.say).toBeCalledWith("Good Morning");
-    expect(output.say).toBeCalledWith("have a great start to the week");
+    expect(output.say).toHaveBeenLastCalledWith(
+      "have a great start to the week"
+    );
+  });
+
+  it("on friday it says good morning, hope you have great plan for the weekend", () => {
+    const output = { say: jest.fn() };
+
+    helloAlexa(output, "Fri");
+    expect(output.say).toBeCalledWith("Good Morning");
+    expect(output.say).toHaveBeenLastCalledWith(
+      "hope you have great plan for the weekend"
+    );
   });
 
   //toHaveBeenLastCalledWithexpect(drink).toHaveBeenNthCalledWith(1, 'lemon');  expect(drink).toHaveBeenNthCalledWith(2, 'octopus');
