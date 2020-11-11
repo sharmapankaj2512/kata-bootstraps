e4978982cc2b518643942e7fd714189d16072564
Tim Bleck <tbl@hellofresh.com>
2020-11-11 14:32:27 +0100

    mob next [ci-skip]

---
+++ js/jest-tests/test/alexa.test.js
@@ -0,0 +1,9 @@
+// helloAlexa returns good morning
+// helloAlexa, on Monday returns good morning, have a great start to the week
+// helloAlexa, on Friday returns good morning, hope you have great plan for the weekend
+
+describe("Alexa", () => {
+  it("says good morning", () => {
+    helloAlexa()
+  })
+})
