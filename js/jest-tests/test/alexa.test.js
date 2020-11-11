1ef2f67afc2f7a8152e785fe4687689905104ff2
Érica Hidemi Mitsuishi <emit@hellofresh.com>
2020-11-11 14:55:56 +0100

    mob next [ci-skip]

--- js/jest-tests/test/alexa.test.js
+++ js/jest-tests/test/alexa.test.js
@@ -2,10 +2,12 @@
 // helloAlexa, on Monday returns good morning, have a great start to the week
 // helloAlexa, on Friday returns good morning, hope you have great plan for the weekend
 
+function helloAlexa(output) {
+  output.say("Good morning");
+}
+
 describe("Alexa", () => {
   it("says good morning", () => {
-    
-    helloAlexa(output)
-    expect(output).toHaveBeenCalledWith("");
-  })
-})
+    helloAlexa({ say: (text) => console.log(text) });
+  });
+});
