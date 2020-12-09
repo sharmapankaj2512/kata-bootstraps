7447f79c046aa6cb5d7e01b16c26e73426c580ee
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2020-12-09 14:31:24 +0100

    mob next [ci-skip]

--- golang/testify/kata_test.go
+++ golang/testify/kata_test.go
@@ -11,39 +11,36 @@ import (
 // xMasTree(2) = uneven number should not work
 // xMasTree(3) = ["_#_", "###", "_#_", "_#_"]
 
-func TestFailing(t *testing.T) {
-	assert.Equal(t, 42, doSomething("a"), "I'm failing you can start with me...")
+func TestEmptyLevelsTree(t *testing.T) {
+	assert.Equal(t, string{"#", "#"}, xMasTree(0))
 }
 
-func TestSomething(t *testing.T) {
-	assert.Equal(t, 42, doSomething("b"), "Answer to the Ultimate Question of Life, the Universe, and Everything")
-}
 
-func TestGen(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    string
-		expected int
-	}{
-		{
-			name:     "with a expected 0",
-			input:    "a",
-			expected: 0,
-		},
-		{
-			name:     "with b expected 42",
-			input:    "b",
-			expected: 42,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := doSomething(tt.input); got != tt.expected {
-				t.Errorf("doSomething() = %v, but expected %v", got, tt.expected)
-			}
-		})
-	}
-}
+//func TestGen(t *testing.T) {
+//	tests := []struct {
+//		name     string
+//		input    string
+//		expected int
+//	}{
+//		{
+//			name:     "with a expected 0",
+//			input:    "a",
+//			expected: 0,
+//		},
+//		{
+//			name:     "with b expected 42",
+//			input:    "b",
+//			expected: 42,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			if got := doSomething(tt.input); got != tt.expected {
+//				t.Errorf("doSomething() = %v, but expected %v", got, tt.expected)
+//			}
+//		})
+//	}
+//}
 
 func doSomething(input string) interface{} {
 	if input == "b" {
