226a8f4340a4a09023b2d9e17ad4278ce7b58fce
Muhammad Sufyian <muhammadsufyian@3350GLOBAL.fritz.box>
2021-01-20 14:29:08 +0100

    mob next [ci-skip]

--- java/junit5/src/test/java/ThingTest.java
+++ java/junit5/src/test/java/ThingTest.java
@@ -11,6 +11,21 @@ public class ThingTest {
         assertEquals("Food", value);
     }
 
+    /*
+    1. If we put echo it output ohce
+    2. If we input a palendrome it reverse and outputs ¡Bonita palabra! ana for example ,
+       input ana would produce ¡Bonita palabra!
+    3. If we input stop , ohce will produce *Adios < your name >*
+    4. If given an input with your name , it should greet with your name
+
+     */
+
+
+    @Test
+    void should_greet_with_name(){
+
+    }
+
     @Test
     void it_should_not_fail() {
         assertTrue(true);
